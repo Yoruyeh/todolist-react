28ab4d9a364b4b8b9b08c708a92a5f0bebed36b3
Yoruyeh <orchardsky31@gmail.com>
2023-05-16 17:03:52 +0800

    add change mode handler

--- src/components/TodoCollection.jsx
+++ src/components/TodoCollection.jsx
@@ -10,10 +10,14 @@ const TodoCollection = ({
   return (
     <div>
       {todos.map(todo => {
-        return <TodoItem key={todo.id} todo={todo} 
-        onToggleDone={(id) => 
-          onToggleDone?.(id)
-        }/>
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggleDone={(id) => onToggleDone?.(id)}
+            onChangeMode={({ id, isEdit }) => onChangeMode?.({ id, isEdit })}
+          />
+        );
       })}
     </div>
   );
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -103,7 +103,7 @@ const StyledTaskItem = styled.div`
 
 const TodoItem = ({ todo, onToggleDone, onSave, onDelete, onChangeMode}) => {
   return (
-    <StyledTaskItem className={clsx('', { done: todo.isDone })}>
+    <StyledTaskItem className={clsx('', { done: todo.isDone, edit: todo.isEdit })}>
       <div className="task-item-checked">
         <span 
         className="icon icon-checked" 
@@ -111,9 +111,11 @@ const TodoItem = ({ todo, onToggleDone, onSave, onDelete, onChangeMode}) => {
           onToggleDone?.(todo.id)
           }} />
       </div>
-      <div className="task-item-body">
+      <div className="task-item-body" onDoubleClick={() => {
+        onChangeMode?.({ id: todo.id, isEdit: true })}
+        }>
         <span className="task-item-body-text">{todo.title}</span>
-        <input className="task-item-body-input" />
+        <input className="task-item-body-input" defaultValue={todo.title} />
       </div>
       <div className="task-item-action ">
         <button className="btn-reset btn-destroy icon"></button>
