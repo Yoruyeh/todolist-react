c97df19d98623febe83ba82e6d0dd9e3645710dc
Yoruyeh <orchardsky31@gmail.com>
2023-05-22 17:09:52 +0800

    add home page navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { isAuthenticated } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/todos')
+    } else {
+      navigate('/login')
+    }
+  }, [navigate, isAuthenticated])
 };
 
 export default HomePage;
