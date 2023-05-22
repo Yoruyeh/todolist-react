39112661617aef9634a30d1779579f3e3886ac31
Yoruyeh <orchardsky31@gmail.com>
2023-05-22 16:41:04 +0800

    export context hook and add auth provider to app

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 import { register, login, checkPermission } from '../api/auth'
 import * as jwt from 'jsonwebtoken'
 import { useLocation } from 'react-router-dom';
@@ -11,7 +11,10 @@ const defaultAuthContext = {
   logout: null,
 };
 
+
 const AuthContext = createContext(defaultAuthContext);
+
+export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
