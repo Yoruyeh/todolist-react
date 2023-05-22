b56093e4cdb62c4d494af6d4fb7849ed5f81fc96
Yoruyeh <orchardsky31@gmail.com>
2023-05-22 15:40:07 +0800

    add default Auth Context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from 'react';
+
+
+const defaultAuthContext = {
+  isAuthenticated: false,
+  currentMember: null,
+  register: null,
+  login: null,
+  logout: null,
+};
+
+const AuthContext = createContext(defaultAuthContext);
+
