de27da1b7111faac7dd9e87400276df4e72c70fc
Yoruyeh <orchardsky31@gmail.com>
2023-05-22 17:15:21 +0800

    add current member at todo page and logout from context

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -10,7 +10,7 @@ const TodoPage = () => {
   const [todos, setTodos] = useState([]);
   const count = todos.length
   const navigate = useNavigate();
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated, currentMember } = useAuth()
 
   const handleChange = (value) => {
     setInputValue(value)
@@ -168,7 +168,7 @@ const TodoPage = () => {
   return (
     <div>
       TodoPage
-      <Header />
+      <Header username={currentMember?.name}/>
       <TodoInput
         inputValue={inputValue}
         onChange={handleChange}
