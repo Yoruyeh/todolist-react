17aa4a1ec0a27ccc68d18e43959739b1c1cbd6e5
Yoruyeh <orchardsky31@gmail.com>
2023-05-22 16:35:42 +0800

    add check permission to context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState } from 'react';
-import { register, login } from '../api/auth'
+import { createContext, useState, useEffect } from 'react';
+import { register, login, checkPermission } from '../api/auth'
 import * as jwt from 'jsonwebtoken'
+import { useLocation } from 'react-router-dom';
 
 const defaultAuthContext = {
   isAuthenticated: false,
@@ -12,41 +13,61 @@ const defaultAuthContext = {
 
 const AuthContext = createContext(defaultAuthContext);
 
-const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [payload, setPayload] = useState(null);
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    const checkTokenIsValid = async () => {
+      const authToken = localStorage.getItem('authToken');
+      if (!authToken) {
+        setIsAuthenticated(false);
+        setPayload(null);
+        return;
+      } 
+      const result = await checkPermission(authToken);
+      if (result) {
+        setIsAuthenticated(true);
+        const tempPayload = jwt.decode(authToken);
+        setPayload(tempPayload);
+      } else {
+        setIsAuthenticated(false);
+        setPayload(null);
+      }
+    };
+    checkTokenIsValid();
+  }, [pathname]);
 
   return (
-    <AuthProvider
+    <AuthContext.Provider
       value={{
         isAuthenticated,
         currentMember: payload && {
           id: payload.sub,
-          name: payload.name
+          name: payload.name,
         },
         register: async (data) => {
-          const { success, authToken } = await 
-          register({
+          const { success, authToken } = await register({
             username: data.username,
             email: data.email,
             password: data.password,
           });
           const tempPayload = jwt.decode(authToken);
           if (tempPayload) {
-            setPayload(tempPayload)
-            setIsAuthenticated(true)
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
             localStorage.setItem('authToken', authToken);
           } else {
-            setPayload(null)
-            setIsAuthenticated(false)
+            setPayload(null);
+            setIsAuthenticated(false);
           }
           return success;
         },
         login: async (data) => {
-          const { success, authToken } = await 
-          login({ 
-            username: data.username, 
-            password: data.password 
+          const { success, authToken } = await login({
+            username: data.username,
+            password: data.password,
           });
           const tempPayload = jwt.decode(authToken);
           if (tempPayload) {
@@ -63,10 +84,10 @@ const AuthProvider = ({ children }) => {
           localStorage.removeItem('authToken');
           setPayload(null);
           setIsAuthenticated(false);
-        }
+        },
       }}
     >
       {children}
-    </AuthProvider>
+    </AuthContext.Provider>
   );
 };
